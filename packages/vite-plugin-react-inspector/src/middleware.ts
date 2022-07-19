45d58c0691e40f1a5ca1c043e87d65a18908356c
Frozen FIsh <976499226@qq.com>
2022-07-19 17:00:08 +0800

    chore: update middleware.ts

---
+++ packages/vite-plugin-react-inspector/src/middleware.ts
@@ -0,0 +1,16 @@
+import type { Connect } from 'vite'
+import { launchEditor } from './launch-editor'
+export const SERVER_URL = '/__react-inspector-launch-editor'
+
+type RequestMessage = Parameters<Connect.NextHandleFunction>[0]
+export const queryParserMiddleware: Connect.NextHandleFunction = (
+  req: RequestMessage & { query?: object },
+  _,
+  next,
+) => {
+  if (!req.query && req.url?.startsWith(SERVER_URL)) {
+    const url = new URL(req.url, 'http://domain.inspector')
+    req.query = Object.fromEntries(url.searchParams.entries())
+  }
+  next()
+}
