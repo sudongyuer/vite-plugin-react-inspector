abe802f5cc6ee53966be2bf6d378a868de0d328a
zhangwc-c <zhangwc-c@glodon.com>
2022-07-23 00:56:40 +0800

    chore: add react 17.x project

---
+++ packages/react-project-18/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import Inspect from 'vite-plugin-inspect'
+import HotExport from 'vite-plugin-hot-export'
+import ReactInspector from 'vite-plugin-react-inspector'
+import svgr from 'vite-plugin-svgr'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [ReactInspector(), Inspect(), react(), svgr({
+    svgrOptions: {
+      icon: true,
+      // ...svgr options (https://react-svgr.com/docs/options/)
+    },
+  })],
+},
+)
