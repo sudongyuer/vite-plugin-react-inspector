0cee92b6bf653a83e9b6297cf6563cb434c5fe7e
alexzhang1030 <1642114555@qq.com>
2022-07-25 14:14:57 +0800

    chore: add test case

--- packages/vite-plugin-react-inspector/test/index.test.ts
+++ packages/vite-plugin-react-inspector/test/index.test.ts
@@ -1,6 +1,16 @@
 import { describe, expect, it } from 'vitest'
+import { parseFilePath } from '../src/utils'
 describe('should', () => {
   it('exported', () => {
     expect(1).toEqual(1)
   })
+})
+
+it('multiple platforms path splice test', () => {
+  const OSX = '/Users/user/Desktop/test/test.jsx:1:1'
+  expect(parseFilePath(OSX)).toEqual(['/Users/user/Desktop/test/test.jsx', '1', '1'])
+  const WINDOWS = 'D:/test/test.jsx:1:1'
+  expect(parseFilePath(WINDOWS)).toEqual(['D:/test/test.jsx', '1', '1'])
+  const LINUX = '/Users/user/Desktop/test/test.jsx:1:1'
+  expect(parseFilePath(LINUX)).toEqual(['/Users/user/Desktop/test/test.jsx', '1', '1'])
 })
