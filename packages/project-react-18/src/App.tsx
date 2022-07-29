d7c9dfb8ec12881dd9c23b4cc5278a9746c0848c
Frozen FIsh <976499226@qq.com>
2022-07-29 15:47:36 +0800

    chore: test React.Fragment

--- packages/project-react-18/src/App.tsx
+++ packages/project-react-18/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import './App.css'
 import Hello from './Hello'
 import ReactInspector from './ReactInspector'
@@ -5,18 +6,20 @@ import ReactInspector from './ReactInspector'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <ReactInspector/>
-        <img
-          style={{ width: '150px' }}
-          src={''}
+      <React.Fragment>
+        <header className="App-header">
+          <ReactInspector/>
+          <img
+            style={{ width: '150px' }}
+            src={''}
          />
-        <p>vite-plugin-react-inspector</p>
-        <p>
-          GitHub 👉 sudongyuer 🐟
-        </p>
-        <Hello/>
-      </header>
+          <p>vite-plugin-react-inspector</p>
+          <p>
+            GitHub 👉 sudongyuer 🐟
+          </p>
+          <Hello/>
+        </header>
+      </React.Fragment>
     </div>
   )
 }
